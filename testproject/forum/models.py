0503a6a77f59eb0a89d96d1f335b66d7295ce1be
da-eu <da1e2u3@gmail.com>
2024-09-18 15:23:26 +0900

    https://daeudaeu.com/django-model/

--- testproject/forum/models.py
+++ testproject/forum/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=32)
+    email = models.EmailField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.username
+    
+class Comment(models.Model):
+    text = models.CharField(max_length=256)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:10]
--- testproject/forum/views.py
+++ testproject/forum/views.py
@@ -1,40 +1,14 @@
 from django.http import Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .forms import RegisterForm, PostForm
-
-class User:
-    def __init__(self, id, username, email, age):
-        self.id = id
-        self.username = username
-        self.email = email
-        self.age = age
-
-import datetime
-class Comment:
-    def __init__(self, id, text, date):
-        self.id = id
-        self.text = text
-        self.date = date
-
-users = [
-    User(1, 'Yamada Taro', 'taro@yamada.jp', 18),
-    User(2, 'Yamada Hanako', 'hanako@yamada.jp', 22),
-    User(3, 'Sato Saburo', 'saburo@sato.jp', 53),
-    User(4, 'Takahashi Shiro', 'shiro@takahashi.jp', 64)
-]
-
-comments = [
-    Comment(1, 'おはようございます', datetime.datetime(2023, 3, 4, 12, 4, 0)),
-    Comment(2, 'いい天気ですねー', datetime.datetime(2023, 4, 5, 16, 21, 0)),
-    Comment(3, '明日もよろしくお願いします', datetime.datetime(2000, 12, 25, 1, 55, 0)),
-    Comment(4, 'おやすみなさい', datetime.datetime(2024, 1, 1, 1, 37, 0)),
-    Comment(5, '山路を登りながら、こう考えた。智に働けば角が立つ。情に棹させば流される。意地を通とおせば窮屈だ。とかくに人の世は住みにくい。', datetime.datetime(2012, 10, 8, 3, 49, 0)),
-]
+from .models import User, Comment
 
 def index_view(request):
     return redirect(to='comments')
 
 def users_view(request):
+    users = User.objects.all()
+
     context = {
         'users' : users
     }
@@ -42,10 +16,7 @@ def users_view(request):
     return render(request, 'forum/users.html', context)
 
 def user_view(request, user_id):
-    if user_id > len(users) or user_id < 1:
-        raise Http404('Not found user')
-
-    user = users[user_id - 1]
+    user = get_object_or_404(User, id=user_id)
 
     context = {
         'user' : user
@@ -54,6 +25,8 @@ def user_view(request, user_id):
     return render(request, 'forum/user.html', context)
 
 def comments_view(request):
+    comments = Comment.objects.all()
+
     context = {
         'comments' : comments
     }
@@ -61,10 +34,7 @@ def comments_view(request):
     return render(request, 'forum/comments.html', context)
 
 def comment_view(request, comment_id):
-    if comment_id > len(comments) and comment_id < 1:
-        raise Http404('Not found comment')
-
-    comment = comments[comment_id - 1]
+    comment = get_object_or_404(Comment, id=comment_id)
 
     context = {
         'comment' : comment
@@ -78,13 +48,13 @@ def register_view(request):
 
         form = RegisterForm(request.POST)
         if form.is_valid():
-            id = len(users) + 1
+
             username = form.cleaned_data.get('username')
             email = form.cleaned_data.get('email')
             age = form.cleaned_data.get('age')
             
-            user = User(id=id, username=username, email=email, age=age)
-            users.append(user)
+            user = User(username=username, email=email, age=age)
+            user.save()
 
             return redirect('users')
 
@@ -102,12 +72,10 @@ def post_view(request):
         form = PostForm(request.POST)
 
         if form.is_valid():
-            id = len(comments) + 1
             text = form.cleaned_data.get('text')
-            date = datetime.datetime.now()   
 
-            comment = Comment(id, text, date)
-            comments.append(comment)
+            comment = Comment(text=text)
+            comment.save()
 
             return redirect('comments')
     else:
