a31b1c49f0c1a8dc1be8620c8dfcf13c39524dd8
da-eu <da1e2u3@gmail.com>
2024-09-29 10:24:00 +0900

    https://daeudaeu.com/django-modeladmin/

--- testproject/forum/admin.py
+++ testproject/forum/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Comment
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+User = get_user_model()
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'user')
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'age')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'age', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'age', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Comment, CommentAdmin)
