fcb07aa2ddb91253acd25a3e1c4764cba229ca12
da-eu <da1e2u3@gmail.com>
2024-12-28 08:33:17 +0900

    https://daeudaeu.com/django-classview/

--- testproject/forum/urls.py
+++ testproject/forum/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index'),
-    path('comments/', views.comments_view, name='comments'),
-    path('comment/<int:comment_id>/', views.comment_view, name='comment'),
-    path('users/', views.users_view, name='users'),
-    path('user/<int:user_id>/', views.user_view, name='user'),
-    path('register/', views.register_view, name='register'),
-    path('post/', views.post_view, name='post'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('', views.Index.as_view(), name='index'),
+    path('comments/', views.CommentList.as_view(), name='comments'),
+    path('comment/<int:comment_id>', views.CommentDetail.as_view(), name='comment'),
+    path('users/', views.UserList.as_view(), name='users'),
+    path('user/<int:user_id>', views.UserDetail.as_view(), name='user'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('post/', views.Post.as_view(), name='post'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
 ]
--- testproject/forum/views.py
+++ testproject/forum/views.py
@@ -1,127 +1,82 @@
-from django.shortcuts import redirect, render, get_object_or_404
 from .forms import RegisterForm, PostForm, LoginForm
 from .models import Comment
-from django.contrib.auth import get_user_model, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView, RedirectView, CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse, reverse_lazy
 
 User = get_user_model()
 
-@login_required
-def index_view(request):
-    return redirect(to='comments')
+class Login(LoginView):
+    form_class = LoginForm
+    template_name = 'forum/login.html'
 
-@login_required
-def users_view(request):
-    users = User.objects.prefetch_related('comments').order_by('date_joined')
+    def get_success_url(self):
+        return reverse('user', kwargs={'user_id':self.request.user.id})
 
-    paginator = Paginator(users, 3)
-    number = int(request.GET.get('p', 1))
-    page_obj = paginator.page(number)
+class Logout(LogoutView):
+    next_page = reverse_lazy('login')
 
-    context = {
-        'page_obj' : page_obj
-    }
+class Index(RedirectView):
+    url = reverse_lazy('comments')
 
-    return render(request, 'forum/users.html', context)
+class UserList(LoginRequiredMixin, ListView):
+    queryset = User.objects.prefetch_related('comments').order_by('date_joined')
+    template_name = 'forum/users.html'
+    paginate_by = 3
+    page_kwarg = 'p'
 
-@login_required
-def user_view(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    comments = Comment.objects.filter(user=user).order_by('date')
+class UserDetail(LoginRequiredMixin, DetailView):
+    model = User
+    context_object_name = 'user'
+    pk_url_kwarg = 'user_id'
+    template_name = 'forum/user.html'
 
-    paginator = Paginator(comments, 3)
-    number = int(request.GET.get('p', 1))
-    page_obj = paginator.page(number)
+    def get_context_data(self, **kwargs):
+        comments = Comment.objects.filter(user=self.object).order_by('date')
 
-    context = {
-        'user' : user,
-        'page_obj' : page_obj
-    }
+        paginator = Paginator(comments, 3)
+        number = int(self.request.GET.get('p', 1))
+        page_obj = paginator.page(number)
 
-    return render(request, 'forum/user.html', context)
+        return super().get_context_data(page_obj=page_obj)
 
-@login_required
-def comments_view(request):
-    comments = Comment.objects.select_related('user').order_by('date')
 
-    paginator = Paginator(comments, 3)
-    number = int(request.GET.get('p', 1))
-    page_obj = paginator.page(number)
+class CommentList(LoginRequiredMixin, ListView):
+    queryset = Comment.objects.select_related('user').order_by('date')
+    paginate_by = 3
+    page_kwarg = 'p'
+    template_name = 'forum/comments.html'
 
-    context = {
-        'page_obj' : page_obj
-    }
 
-    return render(request, 'forum/comments.html', context)
+class CommentDetail(LoginRequiredMixin, DetailView):
+    model = Comment
+    pk_url_kwarg = 'comment_id'
+    template_name = 'forum/comment.html'
 
-@login_required
-def comment_view(request, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
+class Register(CreateView):
+    model = User
+    form_class = RegisterForm
+    template_name = 'forum/register.html'
 
-    context = {
-        'comment' : comment
-    }
-
-    return render(request, 'forum/comment.html', context)
-
-def register_view(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('user', user.id)
-
-    else:
-        form = RegisterForm()
+    def form_valid(self, form):
+        
+        response = super().form_valid(form)
+        user = self.object
+        login(self.request, user)
+        return response
     
-    context = {
-        'form': form
-    }
+    def get_success_url(self):
+        return reverse('user', kwargs={'user_id':self.object.pk})
 
-    return render(request, 'forum/register.html', context)
+class Post(LoginRequiredMixin, CreateView):
+    form_class = PostForm
+    template_name = 'forum/post.html'
+    success_url = reverse_lazy('comments')
 
-@login_required
-def post_view(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-
-        if form.is_valid():
-            comment = form.instance
-            comment.user = request.user
-            comment.save()
-            
-            return redirect('comments')
-    else:
-        form = PostForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'forum/post.html', context)
-
-def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm(request, request.POST)
-
-        if form.is_valid():
-            user = form.get_user()
-
-            if user:
-                login(request, user)
-                return redirect('user', user.id)
-
-    else:
-        form = LoginForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'forum/login.html', context)
-
-def logout_view(request):
-    logout(request)
-    return redirect('login')
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
